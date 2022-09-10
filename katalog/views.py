e4869dcd1a78a5cb3e2a30a2fadbbefb93046bda
monica.shannon <monica.shannon.ms@gmail.com>
2022-09-11 01:37:52 +0700

    first commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang' : data_barang_katalog,
+        'nama' : 'Monica Shannon Clio Aurelie',
+        'npm' : '2106751732'
+    }
+    return render(request, "katalog.html", context)
