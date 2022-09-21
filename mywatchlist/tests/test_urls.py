52bf7242ad1006b5f19b84b8f7839ab9add8d2fb
monica.shannon <monica.shannon.ms@gmail.com>
2022-09-21 16:28:35 +0700

    test

--- mywatchlist/tests/test_urls.py
+++ mywatchlist/tests/test_urls.py
@@ -1,19 +1,16 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
 from mywatchlist.views import show_json, show_xml, show_watchlist
 
 class TestUrls(TestCase):
     def test_html_url(self):
-        client = Client()
-        response = client.get(reverse("mywatchlist:show_watchlist"))
+        response = self.client.get(reverse("mywatchlist:show_watchlist"))
         self.assertEquals(response.status_code, 200)
 
     def test_json_url(self):
-        client = Client()
-        response = client.get(reverse("mywatchlist:show_json"))
+        response= self.client.get(reverse("mywatchlist:show_json"))
         self.assertEquals(response.status_code, 200)
 
     def test_xml_url(self):
-        client = Client()
-        response = client.get(reverse("mywatchlist:show_xml"))
+        response = self.client.get(reverse("mywatchlist:show_xml"))
         self.assertEquals(response.status_code, 200)
