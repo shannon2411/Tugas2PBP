f3a62bce0e136180c0a9c7d1589b78f8cf5b9e8f
monica.shannon <monica.shannon.ms@gmail.com>
2022-10-12 13:15:02 +0700

    tugas 6

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,6 +4,7 @@ from todolist.views import login_user
 from todolist.views import logout_user
 from todolist.views import show_todolist
 from todolist.views import show_create_todo
+from todolist.views import show_json, add_todo
 
 app_name = 'todolist'
 
@@ -16,5 +17,7 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('create-task/', show_create_todo, name='show_create_todo')
+    path('create-task/', show_create_todo, name='show_create_todo'),
+    path('json/', show_json, name='show_json'),
+    path('add/', add_todo, name='add_todo'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -8,8 +8,14 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
 from todolist.models import Task
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 # Create your views here.
+def show_json(request):
+    data_todo = Task.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize("json", data_todo), content_type="application/json")
+
 @login_required(login_url='/todolist/login/')
 def show_todolist(request):
     data_todo = Task.objects.filter(user=request.user)
@@ -62,3 +68,17 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('todolist:login'))
     response.delete_cookie('last_login')
     return response
+
+@csrf_exempt
+def add_todo(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        todo = Task.objects.create(title=title, description=description,date=datetime.date.today(), user=request.user)
+        return JsonResponse({'pk':todo.pk,
+                'fields':{
+                'title':todo.title,
+                'date':todo.date,
+                'description':todo.description,
+            }})
+
